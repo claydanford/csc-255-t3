6f83697dcba238f67ef09f5b0e9a4d6e9a77fe81
Clay Danford <claydanford4@gmail.com>
2026-05-12 18:46:13 -0500

    Little formatting

--- src/board.py
+++ src/board.py
@@ -1,5 +1,6 @@
 from src.mark import Mark
 
+
 class Board:
     def __init__(self):
         self.cells = self._empty_cells()
@@ -13,7 +14,7 @@ class Board:
     def get(self, index: int):
         return self.cells[index]
 
-    def set(self, index, value):
+    def set(self, index: int, value: Mark):
         self.cells[index] = value
 
     def __str__(self):
@@ -22,4 +23,5 @@ class Board:
             "---+---+---\n"
             f" {self.cells[3]} | {self.cells[4]} | {self.cells[5]} \n"
             "---+---+---\n"
-            f" {self.cells[6]} | {self.cells[7]} | {self.cells[8]} ")
+            f" {self.cells[6]} | {self.cells[7]} | {self.cells[8]} "
+        )
